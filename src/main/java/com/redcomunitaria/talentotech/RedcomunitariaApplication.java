50fa99228d40240cbbb6d9dff99fc535410f2b2e
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2026-02-26 19:52:57 -0500

    se crea la estructura del proyecto

---
+++ src/main/java/com/redcomunitaria/talentotech/RedcomunitariaApplication.java
@@ -0,0 +1,13 @@
+package com.redcomunitaria.talentotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedcomunitariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedcomunitariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redcomunitaria/talentotech/RedcomunitariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redcomunitaria.talentotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedcomunitariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
